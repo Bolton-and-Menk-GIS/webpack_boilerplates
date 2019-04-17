3998397233958958817e8929d9fc21695065cf62
Nate Christ <nat1597@hotmail.com>
2019-04-17 15:39:38 -0500

    Update App.vue

--- esri-vue-bootvue/src/App.vue
+++ esri-vue-bootvue/src/App.vue
@@ -35,8 +35,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  height: 750px; /* delete me */
+  margin-top: 10px;
+  height: 100%;
+  width: 100%;
 }
 
 </style>
